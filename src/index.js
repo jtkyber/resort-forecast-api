c76b11e277c9fb9dd16bff8d436a18beab3e9e09
jtkyber <43868043+jtkyber@users.noreply.github.com>
2022-04-21 18:17:21 -0400

    removed heroku timeout bypass

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const NodeCache = require("node-cache");
 const myCache = new NodeCache();
 const cors = require('cors');
 const app = express();
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 app.use(express.json());
 app.use(cors());
 
@@ -16,96 +16,95 @@ const forecast = require('./controllers/forecast');
 const snowConditions = require('./controllers/snowConditions');
 
 let url = null;
-let result = null;
-let myTimer = null;
+// let result = null;
+// let myTimer = null;
 let newUrlCached;
-let bypassTimeoutCount = 0;
-let params;
-let query;
-let mailOptions = {};
+// let bypassTimeoutCount = 0;
+// let params;
+// let query;
+// let mailOptions = {};
 
 //Middleware
 
-const assignMailOptions = () => {
-    const formattedQuery = () => {
-        let queryString = '';
-        for (const [key, value] of Object.entries(query)) {
-            console.log(key, value)
-            queryString = queryString.concat(`${queryString.length ? ' | ' : ''}${key}: ${value}`)
-        }
-        return queryString;
-    }
-    mailOptions = {
-        from: 'resortweatherapi@gmail.com',
-        to: 'resortweatherapi@gmail.com',
-        subject: `'${params?.resort}' forecast retrieval timeout`,
-        html: `<h3>Server reached the maximum amount of time allowed to retrieve forecast information for '${params?.resort}'</h3>
-        <p>Resort Name: ${params?.resort}</p>
-        <p>Query Params: ${formattedQuery()}</p>
-        <p>Url: ${url}/mid</p>`
-    };
-}
+// const assignMailOptions = () => {
+//     const formattedQuery = () => {
+//         let queryString = '';
+//         for (const [key, value] of Object.entries(query)) {
+//             queryString = queryString.concat(`${queryString.length ? ' | ' : ''}${key}: ${value}`)
+//         }
+//         return queryString;
+//     }
+//     mailOptions = {
+//         from: 'resortweatherapi@gmail.com',
+//         to: 'resortweatherapi@gmail.com',
+//         subject: `'${params?.resort}' forecast retrieval timeout`,
+//         html: `<h3>Server reached the maximum amount of time allowed to retrieve forecast information for '${params?.resort}'</h3>
+//         <p>Resort Name: ${params?.resort}</p>
+//         <p>Query Params: ${formattedQuery()}</p>
+//         <p>Url: ${url}/mid</p>`
+//     };
+// }
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'resortweatherapi@gmail.com',
-        pass: 'Potato_16'
-    }
-}); 
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: 'resortweatherapi@gmail.com',
+//         pass: 'Potato_16'
+//     }
+// }); 
 
-const sendEmail = () => {
-    transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-}
+// const sendEmail = () => {
+//     transporter.sendMail(mailOptions, function(error, info) {
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             console.log('Email sent: ' + info.response);
+//         }
+//     });
+// }
 
-const waitAndSend = (req, res) => {
-    if (result) {
-        myCache.set(`${newUrlCached}`, result, 600)
+// const waitAndSend = (req, res) => {
+//     if (result) {
+//         myCache.set(`${newUrlCached}`, result, 600)
 
-        isFinished = true;
-        clearInterval(myTimer);
-        if (!res.headersSent) {
-            res.json(result);
-        } else res.end(JSON.stringify(result, null, 2));
-    } else if (!result) {
-        if (bypassTimeoutCount >= 1) {
-            bypassTimeoutCount = 0;
-            clearInterval(myTimer);
-            sendEmail();
-            res.status(504).json('Could not retrieve forecast information within the allotted time limit');
-            return;
-        }
+//         isFinished = true;
+//         clearInterval(myTimer);
+//         if (!res.headersSent) {
+//             res.json(result);
+//         } else res.end(JSON.stringify(result, null, 2));
+//     } else if (!result) {
+//         if (bypassTimeoutCount >= 1) {
+//             bypassTimeoutCount = 0;
+//             clearInterval(myTimer);
+//             sendEmail();
+//             res.status(504).json('Could not retrieve forecast information within the allotted time limit');
+//             return;
+//         }
 
-        if (!res.headersSent) res.writeHead(202, {'content-type':'application/json'});
-        res.write(" ");
-        bypassTimeoutCount += 1;
-    }
-}
+//         if (!res.headersSent) res.writeHead(202, {'content-type':'application/json'});
+//         res.write(" ");
+//         bypassTimeoutCount += 1;
+//     }
+// }
 
 app.use('/', async (req, res, next) => { 
     url = null;
-    result = null;
-    myTimer = null;
+    // result = null;
+    // myTimer = null;
     next();
 })
 
 app.use('/:resort', async (req, res, next) => {
-    params = req.params;
-    query = req.query;
+    // params = req.params;
+    // query = req.query;
     url = await getUrl.getUrl(req, request, cheerio, myCache);
-    assignMailOptions();
+    // assignMailOptions();
     if (url) {
         newUrlCached = url + Object.values(req.query).sort().toString();
         if (myCache.has(newUrlCached)) {
             res.json(myCache.get(newUrlCached));
         } else {
-            myTimer = setInterval(waitAndSend, 25000, req, res);
+            // myTimer = setInterval(waitAndSend, 25000, req, res);
             next();
         }
     } else {
@@ -118,21 +117,30 @@ app.use('/:resort', async (req, res, next) => {
 app.get('/', (req, res) => { res.json('Working') })
 
 app.get('/:resort/hourly', async (req, res) => { 
-    result = await hourly.hourly(req, res, p, url, myCache);
-    clearInterval(myTimer);
-    myTimer = setInterval(waitAndSend, 100, req, res);
+    const result = await hourly.hourly(req, res, p, url, myCache);
+    myCache.set(`${newUrlCached}`, result, 600)
+
+    res.json(result);
+    // clearInterval(myTimer);
+    // myTimer = setInterval(waitAndSend, 100, req, res);
 })
 
 app.get('/:resort/forecast', async (req, res) => { 
-    result = await forecast.forecast(req, res, p, url, myCache);
-    clearInterval(myTimer);
-    myTimer = setInterval(waitAndSend, 100, req, res);
+    const result = await forecast.forecast(req, res, p, url, myCache);
+    myCache.set(`${newUrlCached}`, result, 600)
+
+    res.json(result);
+    // clearInterval(myTimer);
+    // myTimer = setInterval(waitAndSend, 100, req, res);
 })
 
 app.get('/:resort/snowConditions', async (req, res) => { 
-    result = await snowConditions.snowConditions(req, res, cheerio, request, url, myCache);
-    clearInterval(myTimer);
-    myTimer = setInterval(waitAndSend, 100, req, res);
+    const result = await snowConditions.snowConditions(req, res, cheerio, request, url, myCache);
+    myCache.set(`${newUrlCached}`, result, 600)
+
+    res.json(result); 
+    // clearInterval(myTimer);
+    // myTimer = setInterval(waitAndSend, 100, req, res);
 })
 
 
