de24b21e1fef0dfdf9388ef47a82247d5bd15d2f
jtkyber <43868043+jtkyber@users.noreply.github.com>
2022-12-11 20:25:31 -0500

    cleanup

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const NodeCache = require("node-cache");
 const myCache = new NodeCache();
 const cors = require('cors');
 const app = express();
-const nodemailer = require('nodemailer');
 app.use(express.json());
 app.use(cors());
 
@@ -18,75 +17,16 @@ const snowConditions = require('./controllers/snowConditions');
 const skiMapScrapers = require('./weatherScraper');
 
 let url = null;
+let newUrlCached;
+// let resortName;
 // let result = null;
 // let myTimer = null;
-let newUrlCached;
 // let bypassTimeoutCount = 0;
 // let params;
 // let query;
-let resortName;
-let mailOptions = {};
 
 //Middleware
 
-const assignMailOptions = () => {
-    // const formattedQuery = () => {
-    //     let queryString = '';
-    //     for (const [key, value] of Object.entries(query)) {
-    //         queryString = queryString.concat(`${queryString.length ? ' | ' : ''}${key}: ${value}`)
-    //     }
-    //     return queryString;
-    // }
-
-    mailOptions = {
-        from: 'resortweatherapi@gmail.com',
-        to: 'resortweatherapi@gmail.com',
-        subject: `Invalid Resort Name Entered`,
-        html: `<h3>Resort: ${resortName}</h3>`
-    };
-}
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'resortweatherapi@gmail.com',
-        pass: 'Potato_16'
-    }
-}); 
-
-const sendEmail = () => {
-    transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-}
-
-// const waitAndSend = (req, res) => {
-//     if (result) {
-//         myCache.set(`${newUrlCached}`, result, 600)
-
-//         isFinished = true;
-//         clearInterval(myTimer);
-//         if (!res.headersSent) {
-//             res.json(result);
-//         } else res.end(JSON.stringify(result, null, 2));
-//     } else if (!result) {
-//         if (bypassTimeoutCount >= 1) {
-//             bypassTimeoutCount = 0;
-//             clearInterval(myTimer);
-//             sendEmail();
-//             res.status(504).json('Could not retrieve forecast information within the allotted time limit');
-//             return;
-//         }
-
-//         if (!res.headersSent) res.writeHead(202, {'content-type':'application/json'});
-//         res.write(" ");
-//         bypassTimeoutCount += 1;
-//     }
-// }
 
 app.use('/', async (req, res, next) => { 
     url = null;
@@ -100,8 +40,7 @@ app.use('/:resort', async (req, res, next) => {
     // query = req.query;
     url = await getUrl.getUrl(req, request, cheerio, myCache);
     resortName = req.params.resort;
-    assignMailOptions();
-
+    
     if (url) {
         newUrlCached = url + Object.values(req.query).sort().toString();
         if (myCache.has(newUrlCached)) {
@@ -112,7 +51,6 @@ app.use('/:resort', async (req, res, next) => {
         }
     } else {
         console.log(req.params.resort)
-        sendEmail();
         res.status(400).json('Invalid resort name')
     }
 })
@@ -124,7 +62,7 @@ app.get('/', (req, res) => { res.json('Working') })
 app.get('/:resort/hourly', async (req, res) => { 
     const result = await hourly.hourly(req, res, p, url, myCache);
     myCache.set(`${newUrlCached}`, result, 600)
-
+    
     res.json(result);
     // clearInterval(myTimer);
     // myTimer = setInterval(waitAndSend, 100, req, res);
@@ -133,7 +71,7 @@ app.get('/:resort/hourly', async (req, res) => {
 app.get('/:resort/forecast', async (req, res) => { 
     const result = await forecast.forecast(req, res, p, url, myCache);
     myCache.set(`${newUrlCached}`, result, 600)
-
+    
     res.json(result);
     // clearInterval(myTimer);
     // myTimer = setInterval(waitAndSend, 100, req, res);
@@ -142,17 +80,11 @@ app.get('/:resort/forecast', async (req, res) => {
 app.get('/:resort/snowConditions', async (req, res) => { 
     const result = await snowConditions.snowConditions(req, res, cheerio, request, url, myCache);
     myCache.set(`${newUrlCached}`, result, 600)
-
+    
     res.json(result); 
     // clearInterval(myTimer);
     // myTimer = setInterval(waitAndSend, 100, req, res);
 })
-
-
-
-
-
-
 
 
 //Ski Map Scrapers
@@ -194,3 +126,43 @@ app.get('/ski-map/scrapeOpenSnow', async (req, res) => {
 app.listen(process.env.PORT || 3001, () => {
     console.log(`app is running on port ${process.env.PORT}`);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const waitAndSend = (req, res) => {
+//     if (result) {
+//         myCache.set(`${newUrlCached}`, result, 600)
+
+//         isFinished = true;
+//         clearInterval(myTimer);
+//         if (!res.headersSent) {
+//             res.json(result);
+//         } else res.end(JSON.stringify(result, null, 2));
+//     } else if (!result) {
+//         if (bypassTimeoutCount >= 1) {
+//             bypassTimeoutCount = 0;
+//             clearInterval(myTimer);
+//             res.status(504).json('Could not retrieve forecast information within the allotted time limit');
+//             return;
+//         }
+
+//         if (!res.headersSent) res.writeHead(202, {'content-type':'application/json'});
+//         res.write(" ");
+//         bypassTimeoutCount += 1;
+//     }
+// }
